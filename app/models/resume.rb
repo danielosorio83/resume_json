9a7c8b7ef6053b840d2a16208345330aa5a2d0f3
Daniel Osorio <danielosorio83@gmail.com>
2017-09-24 08:06:19 -0500

    Added code to Resume

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -1,4 +1,10 @@
 class Resume
-  def initialize(params)
+  attr_accessor :json, :message
+
+  def initialize(params = {})
+    file_content = params[:resume].try(:read)
+    self.json = JSON.parse(file_content)
+  rescue => e
+    self.message = e.message
   end
 end
---
+++ spec/models/resume_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe Resume, type: :model do
+
+  it { is_expected.to respond_to(:json) }
+  it { is_expected.to respond_to(:message) }
+
+  describe '#initialize(params)' do
+    let(:params) { { resume: '' } }
+
+    context 'when `JSON.parse` don\'t raise error' do
+      before do
+        allow(JSON).to receive(:parse).and_return('Something')
+      end
+      it 'assigns the parsed json to `json`' do
+        resume = Resume.new(params)
+        expect(resume.json).not_to be_nil
+      end
+    end
+
+    context 'when `JSON.parse` raise error' do
+      let(:error) { 'Some Error' }
+      before do
+        allow(JSON).to receive(:parse).and_raise(error)
+      end
+      it 'assigns the error message to `message`' do
+        resume = Resume.new(params)
+        expect(resume.message).to eq(error)
+      end
+    end
+  end
+end
